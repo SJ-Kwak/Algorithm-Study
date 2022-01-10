7ea9faff2c3decd0da6a9d7bd4f6f45fb058e08d
SJ-Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2022-01-11 00:12:33 +0900

    학점계산 (진행중)

---
+++ BOJ/Math/2754.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char s[2];
+	scanf("%c%c", &s[0], &s[1]);
+	
+	switch (s[0]) {
+	case 'A': {
+		if (s[1] == '+')
+			printf("4.3");
+		if (s[1] == '0')
+			printf("4.0");
+		if (s[1] == '-')
+			printf("3.7");
+		break;
+	}
+	case 'B': {
+		if (s[1] == '+')
+			printf("4.3");
+		if (s[1] == '0')
+			printf("4.0");
+		if (s[1] == '-')
+			printf("3.7");
+		break;
+	}
+	case 'C': {
+		if (s[1] == '+')
+			printf("4.3");
+		if (s[1] == '0')
+			printf("4.0");
+		if (s[1] == '-')
+			printf("3.7");
+		break;
+	}
+	case 'D': {
+		if (s[1] == '+')
+			printf("4.3");
+		if (s[1] == '0')
+			printf("4.0");
+		if (s[1] == '-')
+			printf("3.7");
+		break;
+	}
+	case 'F': printf("0.0"); break;
+	}
+}
