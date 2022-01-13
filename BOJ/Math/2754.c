5a62ba5b7cbfc74a5c4563d90e71859afaa0e873
SJ-Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2022-01-13 23:57:27 +0900

    학점계산

--- BOJ/Math/2754.c
+++ BOJ/Math/2754.c
@@ -17,29 +17,29 @@ int main() {
 	}
 	case 'B': {
 		if (s[1] == '+')
-			printf("4.3");
+			printf("3.3");
 		if (s[1] == '0')
-			printf("4.0");
+			printf("3.0");
 		if (s[1] == '-')
-			printf("3.7");
+			printf("2.7");
 		break;
 	}
 	case 'C': {
 		if (s[1] == '+')
-			printf("4.3");
+			printf("2.3");
 		if (s[1] == '0')
-			printf("4.0");
+			printf("2.0");
 		if (s[1] == '-')
-			printf("3.7");
+			printf("1.7");
 		break;
 	}
 	case 'D': {
 		if (s[1] == '+')
-			printf("4.3");
+			printf("1.3");
 		if (s[1] == '0')
-			printf("4.0");
+			printf("1.0");
 		if (s[1] == '-')
-			printf("3.7");
+			printf("0.7");
 		break;
 	}
 	case 'F': printf("0.0"); break;
